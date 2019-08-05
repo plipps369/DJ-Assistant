abb3832584573dc94d74ec680e17da460e4de28b
Mark Mustard <colonel3mustard@gmail.com>
2019-08-05 14:31:17 -0400

    navbar done header started

---
+++ dj-assistant/src/components/NavHeader.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <header>
+      <div class="container-fluid">
+            <div class="col-lg-12">
+                <h1></h1>
+            </div>
+        </div>
+    </header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">DJ App</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <router-link :to="{name: 'home'}" class="nav-link">
+              Home
+              <span class="sr-only">(current)</span>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "nav-header"
+};
+</script>
+
+<style>
+nav {
+  position: sticky;
+  overflow: hidden;
+  top: 0;
+  width: 100%; 
+  z-index: 1000;
+}
+</style>
