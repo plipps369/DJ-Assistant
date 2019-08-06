5e873c3d6f22ee50ea53c17b0a68e6bdf905d708
Mark Mustard <colonel3mustard@gmail.com>
2019-08-06 14:08:51 -0400

    dash work

--- dj-assistant/src/views/Dashboard.vue
+++ dj-assistant/src/views/Dashboard.vue
@@ -1,26 +1,47 @@
 <template>
-    <div>
-        <nav-header></nav-header>
-        <div id="dash-main">
-        <h1>THIS IS THE DASHBOARD YA'LL</h1>
-        </div>
+  <div>
+    <nav-header></nav-header>
+    <div id="dash-main">
+      <h1>THIS IS THE DASHBOARD YA'LL</h1>
+      <ul>
+        <!-- <li v-for="party in parties" :key="party.id">{{party.name}}</li> -->
+      </ul>
     </div>
+  </div>
 </template>
 
 <script>
-import NavHeader from '@/components/NavHeader.vue'
+import { APIService } from "@/APIService";
+import NavHeader from "@/components/NavHeader.vue";
+const apiService = new APIService();
 
 export default {
-  name: 'dashboard',
+  name: "dashboard",
+  data() {
+    return {
+      parties: []
+    };
+  },
   components: {
     NavHeader
   },
-  
-}
+  methods: {
+    getPartiesByDJid(DJid) {
+      apiService
+        .getParties(DJid)
+        .then(json => {
+          this.parties = json;
+        })
+        .catch(error => {
+          window.console.log("Error:", error);
+        });
+    }
+  }
+};
 </script>
 
 <style>
-#dash-main{
-    margin-top:15vh;
+#dash-main {
+  margin-top: 15vh;
 }
 </style>
